24860e06d95ad3333acb1e2a16796d11fb209313
Oleksii Dykan <alick_bass@mail.ru>
2017-09-04 12:30:17 +0200

    Add project

---
+++ ReRouter/ReRouter.h
@@ -0,0 +1,19 @@
+//
+//  ReRouter.h
+//  ReRouter
+//
+//  Created by Oleksii on 04/09/2017.
+//  Copyright © 2017 WeAreReasonablePeople. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReRouter.
+FOUNDATION_EXPORT double ReRouterVersionNumber;
+
+//! Project version string for ReRouter.
+FOUNDATION_EXPORT const unsigned char ReRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReRouter/PublicHeader.h>
+
+
