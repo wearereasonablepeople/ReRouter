2e39a788e7a0fc3b7793b6faa4ff922c27155a7e
Oleksii Dykan <alick_bass@mail.ru>
2017-09-05 14:09:02 +0200

    Add complexity info about path methods

--- ReRouter/Path.swift
+++ ReRouter/Path.swift
@@ -24,20 +24,24 @@ public struct Path<Initial: PathIdentifier>: Equatable {
         self.sequence = sequence
     }
     
+    /// Add path id. Complexity: O(1)
     public mutating func append<T: PathIdentifier>(_ id: T) {
         sequence.append(id)
     }
     
+    /// Remove last path id. Complexity: O(1)
     public mutating func removeLast() {
         sequence.removeLast()
     }
     
+    /// Create new path with additional element. Complexity: O(n)
     public func push<T: PathIdentifier>(_ id: T) -> Path<Initial> {
         var new = self
         new.append(id)
         return new
     }
     
+    /// Create new path without last element. Complexity: O(n)
     public func dropLast() -> Path<Initial> {
         var new = self
         new.removeLast()
